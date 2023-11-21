2c8bd62b0a95300c6f8441992c218035bfd893bf
Maksym.Tatariants <Maksym.Tatariants@toshibagcs.com>
2023-11-21 16:25:35 +0200

    implement correct concatentation, add tests

--- src/ops_nalgebra.rs
+++ src/ops_nalgebra.rs
@@ -7,7 +7,7 @@ use nalgebra::{DMatrix, Scalar};
 
 pub fn concat<T: Scalar + Clone + num_traits::Zero>(
     matrix_1: DMatrix<T>, matrix_2: DMatrix<T>, axis: u8,
-) -> Result<(DMatrix<T>), Box<dyn Error>> {
+) -> Result<DMatrix<T>, Box<dyn Error>> {
     let n_rows: usize;
     let n_cols: usize;
 
@@ -34,10 +34,29 @@ pub fn concat<T: Scalar + Clone + num_traits::Zero>(
         n_rows = matrix_1.nrows();
         n_cols = matrix_1.ncols() + matrix_2.ncols();
     }
-    let mut matrix_concat: DMatrix::<T> = DMatrix::from_fn(n_rows, n_cols, |i, j| T::zero());
+    let mut matrix_concat: DMatrix::<T> = DMatrix::from_fn(n_rows, n_cols, |_i, _j| T::zero());
+
     for (idx_row, row) in matrix_1.row_iter().enumerate() {
-        for (idx_col, elem) in row.row_iter().enumerate() {
+        for (idx_col, elem) in row.column_iter().enumerate() {
             matrix_concat[(idx_row, idx_col)] = elem[(0, 0)].clone();
+        }
+    }
+
+    for (idx_row, row) in matrix_2.row_iter().enumerate() {
+        let idx_row_concat: usize;
+        if axis == 0 {
+            idx_row_concat = idx_row + matrix_1.nrows();
+        } else {
+            idx_row_concat = idx_row;
+        }
+        for (idx_col, elem) in row.column_iter().enumerate() {
+            let idx_col_concat: usize;
+            if axis == 0 {
+                idx_col_concat = idx_col;
+            } else {
+                idx_col_concat = idx_col + matrix_1.ncols();
+            }
+            matrix_concat[(idx_row_concat, idx_col_concat)] = elem[(0, 0)].clone();
         }
     }
     return Ok(matrix_concat);
--- tests/test_ops_nalgebra.rs
+++ tests/test_ops_nalgebra.rs
@@ -1,32 +1,79 @@
 extern crate nalgebra;
+extern crate log;
 
+use log::{error, info};
 use nalgebra::{DMatrix};
 
 #[path = "../src/ops_nalgebra.rs"]
 mod ops_nalgebra;
 
-pub fn test_concat() {
-    let axis = 1;
-    let mat_1 = DMatrix::<f64>::from_row_slice(3, 1, &[5., 5., 5.]);
-    let mat_2 = DMatrix::<f64>::from_row_slice(3, 2, &[10., 10., 10., 11., 11., 11.]);
+pub fn test_concat_col() {
+    let mat_1 = DMatrix::<f64>::from_column_slice(4, 1, &[5., 5., 5., 5.]);
+    let mat_2 = DMatrix::<f64>::from_column_slice(4, 2, &[10., 66., 66., 10., 11., 11., 11., 11.]);
 
-    let mat_concat = ops_nalgebra::concat(mat_1, mat_2, axis);
+    let mat_expected: DMatrix::<f64> = DMatrix::from_column_slice(
+        4, 3, &[5., 5., 5., 5., 10., 66., 66., 10., 11., 11., 11., 11.],
+    );
+    let mat_concat = ops_nalgebra::concat(mat_1, mat_2, 1);
+
     match mat_concat {
-        Ok(mat_concat) => println!("{}", mat_concat),
-        Err(e) => println!("An error occurred: {}", e),
+        Ok(mat_concat) => {
+            info!("Matrix concatenation along COL(1) axis: {}", mat_concat);
+            assert_eq!(mat_expected, mat_concat);
+        }
+        Err(e) => error!("An error occurred: {}", e),
     }
 }
 
-pub fn add_two(a: i32) -> i32 {
-    a + 2
+pub fn test_concat_row() {
+    let mat_1 = DMatrix::<f64>::from_column_slice(
+        4, 2, &[5., 5., 5., 5., 6., 6., 6., 6.],
+    );
+    let mat_2 = DMatrix::<f64>::from_column_slice(
+        4, 2, &[10., 66., 66., 10., 11., 11., 11., 11.],
+    );
+    let mat_expected: DMatrix::<f64> = DMatrix::from_column_slice(
+        8, 2, &[
+            5., 5., 5., 5., 10., 66., 66., 10.,
+            6., 6., 6., 6., 11., 11., 11., 11.
+        ],
+    );
+    let mat_concat = ops_nalgebra::concat(mat_1, mat_2, 0);
+
+    match mat_concat {
+        Ok(mat_concat) => {
+            info!("Matrix concatenation along ROW(0) axis: {}", mat_concat);
+            assert_eq!(mat_expected, mat_concat);
+        }
+        Err(e) => error!("An error occurred: {}", e),
+    }
 }
+
 
 #[cfg(test)]
 mod tests {
-    use super::test_concat;
+    use super::test_concat_row;
+    use super::test_concat_col;
+    use std::sync::Once;
+
+    static INIT: Once = Once::new();
+
+    fn setup() {
+        INIT.call_once(|| {
+            std::env::set_var("RUST_LOG", "info");
+            env_logger::init();
+        });
+    }
 
     #[test]
-    fn run_test_concat() {
-        test_concat();
+    fn run_test_concat_col() {
+        setup();
+        test_concat_col();
+    }
+
+    #[test]
+    fn run_test_concat_row() {
+        setup();
+        test_concat_row();
     }
 }
